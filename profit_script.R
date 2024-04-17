4e09a0926310e24c4abd4e51639c4f9054433e08
floraham <floraham@outlook.com>
2024-04-16 20:38:50 -0700

    adding profit script

---
+++ profit_script.R
@@ -0,0 +1,50 @@
+##Profit calculation 
+#' Calculating almond profit
+#'
+#' @param climate_data 
+#' @param baseline_profit 
+#' @param acres 
+#' @param unit_price 
+#' @param unit_cost 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+#--------------probably delete this ---------
+# source("~/Documents/2_Areas/Areas_MEDS/Spring_Quarter/EDS230_Env_mod/homework/almond_hws/almond_yield/almond_model_flora.R")
+# 
+# #load climate data
+# climate_data <- read.csv(here("clim.csv")) %>% data.frame()
+#---------------------------------------------
+
+calculate_profit <- function(climate_data, 
+                              baseline_profit = 20000, 
+                              acres = 300, 
+                              unit_price = 4000, 
+                              unit_cost = 3950) {
+  require(dplyr)
+  require(here)
+  require(tidyverse)
+  
+  #calculate yield_anomaly using the almond_yield script 
+  per_acre_yield <- almond_yield(feb_min_T, jan_precip_sum)
+  
+  #calculate the total_yield_anomaly of all the acres (in tons)
+  total_yield <- per_acre_yield * acres
+  
+  #calculate the expected profit
+  gross_profit = baseline_profit + (total_yield * unit_price) - (total_yield * unit_cost)
+
+  return(list(
+    gross_profit = gross_profit,
+    mean_profit = mean(gross_profit, na.rm = TRUE)
+  ))
+}
+#calculate_profit(climate_data)
+
+
+
+
