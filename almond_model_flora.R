06662a60f9bb5b3e5e22897a5fc8731a9da4b2ff
Flora Shu-Qi Hamilton <34722055+floraham@users.noreply.github.com>
2024-04-17 23:01:12 -0700

    this is the right one

--- almond_model_flora.R
+++ almond_model_flora.R
@@ -1,6 +1,6 @@
 #' simple model of almond yield anomaly response to climate
 #'
-#' @param clim climate data csv
+#' @param climate_data 
 #' @param min_T_feb_coef  coefficient of daily times series of min T in Feb between years 1988-2010, default=-0.015
 #' @param min_T_feb_coefsq default=-0.0046
 #' @param precip_jan_coef coefficient of daily times series of sum precip in Jan between years 1988-2010, default = -0.07 
@@ -12,26 +12,16 @@
 #'
 #' @examples
 
-#Inputs: daily times series of minimum, maximum daily Temperatures and precipitation
-#Outputs: maximum, minimum and mean yield anomoly for a input time series
 
-##attach libraries 
-library(here)
-library(tidyverse)
-
-#load climate data
-climate_data <- read.csv("~/Documents/2_Areas/Areas_MEDS/Spring_Quarter/EDS230_Env_mod/homework/assignment2/almond_yield/clim.csv") %>% data.frame()
-
-almond_yield <- function(clim) {
-  
-  #extract_min_T_feb from climate data df
+almond_yield <- function(climate_data) {
   min_temp <- climate_data %>% group_by(month, year) %>% summarize(min_T = min(tmin_c), .groups = "drop")
-  feb_min_T <- (min_temp %>% filter(month == 2) %>% select(min_T))$min_T
+  feb_min_T <- ((min_temp %>% filter(month == 2) %>% select(min_T)))$min_T
+  years <- unique(min_temp$year)
   
-  #extract_precip_jan from climate data df.... QUESTION: IS PRECIPITATION A MEAN OR A SUM?? 
-  
+  #extract_precip_jan from climate data df....
   precip_sum <- climate_data %>% group_by(month, year) %>% summarize(sum_precip = sum(precip), .groups = "drop")
   jan_precip_sum <- (precip_sum %>% filter(month == 1))$sum_precip 
+
   
   #calculation equation
   al_yield = 
@@ -40,16 +30,9 @@ almond_yield <- function(clim) {
     -0.07 * (jan_precip_sum) +
     0.0043 * ((jan_precip_sum) ^ 2) + 
     0.28
-  
-  #find max, min, mean of yields 
-  maxy = max(al_yield)
-  miny = min(al_yield)
-  meany = mean(al_yield)
-  
-  #return this list so that we can check answers w results 
-  return(list(al_yield, maxy, miny, meany))
+
+  return(yield = al_yield)
 }
 
-almond_yield(clim) 
 
 
